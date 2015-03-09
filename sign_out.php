0b2921ad02bda5d41fdec5a9a52030a407833ffc
grath90 <grath90@gmail.com>
2015-03-09 15:09:05 -0500

    Create sign_out.php

---
+++ sign_out.php
@@ -0,0 +1,14 @@
+<?php
+//sign_out.php
+
+session_start();
+	
+	unset($_SESSION['user_id']);
+	unset($_SESSION['username']);
+	
+	/*setcookie('user_id', '', time()-(365*24*60*60));
+	setcookie('username', '', time()-(365*24*60*60));*/
+	
+	header('Location: sign_in.php');
+
+?>
