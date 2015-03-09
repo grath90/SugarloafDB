37b7c4da1a264b76aac27050233f5162d746e2c6
grath90 <grath90@gmail.com>
2015-03-09 15:03:14 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+ 	require_once 'util/SHC_config.php';
+ 	require_once 'util/view_shc.php';
+ 	
+page_start("Welcome");
+?>
+	<div class="bs-example">
+		<div class="form-group">
+			<div class="col-xs-offset-2 col-xs-10">
+				<a href="util/sign_in.php" target="_blank"><button type="submit" class="btn btn-primary" name="submit">Log In</button></a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
